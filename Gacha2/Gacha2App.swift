32f8109761ed07221b4da2b695aa69b0d3e3dec4
Aoi-Kobayashi <oosabaaoi@gmail.com>
2025-07-21 13:37:01 +0900

    Initial Commit

---
+++ Gacha2/Gacha2App.swift
@@ -0,0 +1,17 @@
+//
+//  Gacha2App.swift
+//  Gacha2
+//
+//  Created by AoiKobayashi on 2025/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Gacha2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
